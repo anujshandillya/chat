19daf1d5f0d3a6ff21bc492362f4fd49cbf1b50e
Anuj Sharma <anujsharmawork11@gmail.com>
2024-03-09 18:13:46 +0530

    fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return <SignUp />
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  )
 }
+
+export default Layout
